51449f4fbcbd42c254cac851e74226ad31f89b9c
unknown <j.marlin@outlook.com>
2022-10-17 18:24:12 -0400

    Seek working, committing before massive codectomy

--- link.ps1
+++ link.ps1
@@ -1,5 +1,5 @@
-ca65 basic.asm -o basic.o
-ld65 -o basic.bin -C link.cfg basic.o
+ca65 -g basic.asm -o basic.o
+ld65 -vm -m symbols.txt -o basic.bin -C link.cfg basic.o
 cp basic.bin tools\injektor\
 cd tools\injektor\
 dotnet run
